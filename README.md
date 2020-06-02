9cfce599718cc1befa6bfabef1fc81d7e55ecc76
jorge <jorherrera87@gmail.com>
2020-06-02 00:34:36 -0400

    Primer commit de restserver

---
+++ README.md
@@ -0,0 +1,5 @@
+## node restserver curso.-
+
+```
+npm install
+```
---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+
+
+ /*
+  PUERTO, process siempre esta corriendo es parte de node.
+ */ 
+process.env.PORT = process.env.PORT || 3000;
+
